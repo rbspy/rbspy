8854c8552cb8924ee53561fc3a707cfb52838af2
Julia Evans <julia@jvns.ca>
2016-05-09 14:00:41 -0400

    take the syscall args from perf top

--- perf-syscall.c
+++ perf-syscall.c
@@ -27,13 +27,31 @@ main(int argc, char **argv)
    long long count;
    int fd;
 
+//   $22 = {type = 0, size = 96, config = 0, {sample_period = 4000, sample_freq =
+// 4000}, sample_type = 391, read_format = 0, disabled = 1,    inherit = 1,
+// pinned = 0, exclusive = 0, exclude_user = 0, exclude_kernel = 0, exclude_hv =
+// 0, exclude_idle = 0, mmap = 1, comm = 1,    freq = 1, inherit_stat = 0,
+// enable_on_exec = 0, task = 0, watermark = 0, precise_ip = 0, mmap_data = 0,
+// sample_id_all = 1,    exclude_host = 0, exclude_guest = 1, __reserved_1 = 0,
+// {wakeup_events = 0, wakeup_watermark = 0}, bp_type = 0, {bp_addr = 0, config1
+// = 0},    {bp_len = 0, config2 = 0}, branch_sample_type = 0}
+ 
+
    memset(&pe, 0, sizeof(struct perf_event_attr));
    pe.type = PERF_TYPE_HARDWARE;
    pe.size = sizeof(struct perf_event_attr);
-   pe.config = PERF_COUNT_HW_INSTRUCTIONS;
+   pe.config = PERF_COUNT_HW_CPU_CYCLES;
+   pe.sample_period = 4000;
+   pe.sample_freq = 4000;
+   pe.sample_type = 391; // ??????? what does it meeeeeeeeeeean
    pe.disabled = 1;
+   pe.inherit = 1;
+   pe.mmap = 1;
+   pe.comm = 1;
+   pe.freq = 1;
+   pe.sample_id_all = 1;
+   pe.exclude_guest = 1;
    pe.exclude_kernel = 1;
-   pe.exclude_hv = 1;
 
    fd = perf_event_open(&pe, 0, -1, -1, 0);
    if (fd == -1) {
