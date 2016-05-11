05b275879ee11defb1534d5d0a140f88259799f5
Julia Evans <julia@jvns.ca>
2016-05-11 09:19:29 -0400

    make a copy_address

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use libc::*;
 use std::env;
 
 // void * copy_address(void* addr, int length, pid_t pid) {
-//     int amount_to_copy = 1000;
 //     void * copy = malloc(length);
 //     struct iovec local_iov;
 //     local_iov.iov_base = copy;
@@ -22,10 +21,24 @@ use std::env;
 //     return copy;
 // }
 
+fn copy_address(addr: *mut c_void, length: usize, pid: pid_t) -> Vec<u8> {
+    let mut copy = vec![0;length];
+    let local_iov = iovec {
+        iov_base: copy.as_mut_ptr() as *mut c_void,
+        iov_len: length
+    };
+    let remote_iov = iovec {
+        iov_base: addr,
+        iov_len: length
+    };
+    unsafe {
+        process_vm_readv(pid, &local_iov, 1, &remote_iov, 1, 0);
+    }
+    copy
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     let pid: pid_t = args[1].parse().unwrap();
-    println!("hello {}!\n", pid);
-    let size_to_copy = 1000;
-
+    println!("pid is {}!\n", pid);
 }
