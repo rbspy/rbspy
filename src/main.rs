aff045e4638159d5870545df86455c5dd25c1950
Julia Evans <julia@jvns.ca>
2016-05-11 10:19:27 -0400

    make ruby_vm compile!!!

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,7 @@
 extern crate libc;
 use libc::*;
 use std::env;
-
-// void * copy_address(void* addr, int length, pid_t pid) {
-//     void * copy = malloc(length);
-//     struct iovec local_iov;
-//     local_iov.iov_base = copy;
-//     local_iov.iov_len = length;
-//     unsigned long liovcnt = 1;
-//     struct iovec remote_iov;
-//     remote_iov.iov_base = addr;
-//     remote_iov.iov_len = length;
-//     unsigned long riovcnt = 1;
-//     process_vm_readv(pid,
-//         &local_iov,
-//         liovcnt,
-//         &remote_iov,
-//         riovcnt,
-//         0);
-//     return copy;
-// }
+mod ruby_vm;
 
 fn copy_address(addr: *mut c_void, length: usize, pid: pid_t) -> Vec<u8> {
     let mut copy = vec![0;length];
