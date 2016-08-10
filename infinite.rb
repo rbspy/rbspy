9cfa8aa4a47228b4eb9aaee105b1ee8eea80eda9
Julia Evans <julia@jvns.ca>
2016-08-10 11:59:51 -0400

    infinite test program

---
+++ infinite.rb
@@ -0,0 +1,19 @@
+def aaaaaaaaaaaaaa()
+  while true
+    2+2
+  end
+end
+def bbbbbbbbbb()
+    aaaaaaaaaaaaaa()
+end
+def cccccccccccccccccccccc()
+  bbbbbbbbbb()
+end
+def ddddddddddddddddddddd()
+  cccccccccccccccccccccc()
+end
+def ggggggggggggggggg()
+  ddddddddddddddddddddd()
+end
+
+ggggggggggggggggg()
