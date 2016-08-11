e944201c8925d8e47e58bd719dc365de990e5de3
Julia Evans <julia@jvns.ca>
2016-08-10 23:59:26 -0400

    statically link!

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 command=`cargo build --release --verbose 2>&1 | tee /tmp/out | grep cc | grep note | grep -Eo cc.+ | sed 's/"//g' | grep stacktrace | sed 's/-pie//'`
 cat /tmp/out
 set -x
-eval "$command -lelf"
+eval "$command -l:libelf.a -l:libz.a"
