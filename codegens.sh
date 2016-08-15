46bec93533907e275dd490e8828cd95a05559734
Julia Evans <julia@jvns.ca>
2016-08-14 21:58:30 -0400

    remove unnecessary codegen scripts

--- codegens.sh
+++
@@ -1,9 +0,0 @@
-python codegen.py dwarf_whatform Dwarf_Attribute Dwarf_Half > out
-python codegen.py dwarf_whatform_direct Dwarf_Attribute Dwarf_Half >> out
-python codegen.py dwarf_whatattr Dwarf_Attribute Dwarf_Half >> out
-python codegen.py dwarf_formref Dwarf_Attribute Dwarf_Off >> out
-python codegen.py dwarf_global_formref Dwarf_Attribute Dwarf_Off >> out
-python codegen.py dwarf_formaddr Dwarf_Attribute Dwarf_Addr >> out
-python codegen.py dwarf_formflag Dwarf_Attribute Dwarf_Bool >> out
-python codegen.py dwarf_formudata Dwarf_Attribute Dwarf_Unsigned >> out
-python codegen.py dwarf_formsdata Dwarf_Attribute Dwarf_Signed >> out
