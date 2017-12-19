b539fd00ef89876a8844d9e1f67b12193fb657f3
Julia Evans <julia@jvns.ca>
2017-12-18 19:03:32 -0500

    update build

--- build.rs
+++ build.rs
@@ -1,9 +1,38 @@
-extern crate rustc_version;
+extern crate bindgen;
 
-use rustc_version::{Channel, version_meta};
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
-    if let Channel::Nightly = version_meta().channel {
-        println!("cargo:rustc-cfg=rustc_nightly");
-    }
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    println!("cargo:rustc-link-lib=bz2");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("wrapper.h")
+        .whitelisted_type("rb_iseq_constant_body")
+        .whitelisted_type("rb_iseq_location_struct")
+        .whitelisted_type("rb_thread_struct")
+        .whitelisted_type("rb_iseq_struct")
+        .whitelisted_type("rb_control_frame_struct")
+        .whitelisted_type("rb_thread_struct")
+        .whitelisted_type("VALUE")
+        .clang_arg("-I/home/bork/.rbenv/versions/2.1.6/include/ruby-2.1.0/")
+        .clang_arg("-I/home/bork/.rbenv/versions/2.1.6/include/ruby-2.1.0/x86_64-linux")
+        .generate_comments(false)
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
