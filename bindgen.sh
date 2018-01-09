8345b84feeaa36efe95a88d415a75f6c70ac044b
Julia Evans <julia@jvns.ca>
2018-01-09 17:29:07 -0500

    Add bindings for Ruby 1.9.1

--- bindgen.sh
+++ bindgen.sh
@@ -1,5 +1,11 @@
 set -eux
 echo "#include </tmp/headers/$1/vm_core.h>" > /tmp/wrapper.h
+rm -rf /tmp/headers/$1
+mkdir -p /tmp/headers/$1
+cd ~/clones/ruby
+git checkout v$1
+cp -R include /tmp/headers/$1
+cp *.h /tmp/headers/$1
 OUT=src/bindings/ruby_${1}.rs
 bindgen /tmp/wrapper.h \
     -o /tmp/bindings.rs \
@@ -8,17 +14,20 @@ bindgen /tmp/wrapper.h \
     --whitelist-type rb_iseq_constant_body \
     --whitelist-type rb_iseq_location_struct \
     --whitelist-type rb_thread_struct \
+    --whitelist-type rb_thread_t \
     --whitelist-type rb_iseq_struct \
     --whitelist-type rb_control_frame_struct \
     --whitelist-type rb_thread_struct \
     --whitelist-type RString \
     --whitelist-type VALUE \
     -- \
-    -I/home/bork/scratch/ruby-header-files/$1/include \
-    -I/home/bork/scratch/ruby-header-files/general -I/home/bork/scratch/ruby-header-files/$1/ \
+    -I/tmp/headers/$1/include \
+    -I/home/bork/scratch/ruby-header-files/general -I/tmp/headers/$1/ \
     -I/usr/lib/llvm-3.8/lib/clang/3.8.0/include/
 
 #rustfmt --force src/bindings/ruby_${1}.rs
+
+cd ~/work/ruby-stacktrace
 
 echo "#![allow(non_upper_case_globals)]" > $OUT
 echo "#![allow(non_camel_case_types)]" >> $OUT
---
+++ examples/infinite.rb
@@ -0,0 +1,15 @@
+def aaa() 
+    sleep(0.5)
+end
+
+def bbb() 
+    aaa()
+end
+
+def ccc() 
+    bbb()
+end
+
+loop do
+    ccc()
+end
