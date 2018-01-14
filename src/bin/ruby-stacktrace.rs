db9ef43c9d51de831e8283f976cf2837a5400f07
Julia Evans <julia@jvns.ca>
2018-01-14 17:04:43 -0500

    Add partial support for printing line numbers!

--- src/bin/ruby-stacktrace.rs
+++ src/bin/ruby-stacktrace.rs
@@ -141,7 +141,8 @@ fn do_main() -> Result<(), Error> {
         process::exit(1);
     }
 
-    let ruby_current_thread_address_location = address_finder::current_thread_address_location(pid, &version)?;
+    let ruby_current_thread_address_location =
+        address_finder::current_thread_address_location(pid, &version)?;
     let stack_trace_function = stack_trace::get_stack_trace_function(&version);
 
     if command == "parse" {
@@ -152,7 +153,7 @@ fn do_main() -> Result<(), Error> {
         loop {
             let trace = stack_trace_function(ruby_current_thread_address_location, pid);
             match trace {
-                Err(copy::MemoryCopyError::ProcessEnded) => { return Ok(()) },
+                Err(copy::MemoryCopyError::ProcessEnded) => return Ok(()),
                 _ => {
                     user_interface::print_stack_trace(&trace?);
                     thread::sleep(Duration::from_millis(10));
@@ -160,38 +161,6 @@ fn do_main() -> Result<(), Error> {
             }
         }
     } else {
-        // top subcommand!
-        // keeps a running histogram of how often we see every method
-        // and periodically reports 'self' and 'total' time for each method
-        let mut method_stats = HashMap::new();
-        let mut method_own_time_stats = HashMap::new();
-        let mut j = 0;
-        loop {
-            j += 1;
-            let trace = stack_trace_function(ruby_current_thread_address_location, pid)
-                .unwrap_or_else(|_| {
-                    process::exit(0);
-                });
-            // only count each element in the stack trace once
-            // otherwise recursive methods are overcounted
-            let mut seen = HashSet::new();
-            for item in &trace {
-                if !seen.contains(&item.clone()) {
-                    let counter = method_stats.entry(item.clone()).or_insert(0);
-                    *counter += 1;
-                }
-                seen.insert(item.clone());
-            }
-            {
-                let counter2 = method_own_time_stats.entry(trace[0].clone()).or_insert(0);
-                *counter2 += 1;
-            }
-            if j % 100 == 0 {
-                user_interface::print_method_stats(&method_stats, &method_own_time_stats, 30);
-                method_stats = HashMap::new();
-                method_own_time_stats = HashMap::new();
-            }
-            thread::sleep(Duration::from_millis(10));
-        }
+        return Ok(())
     }
 }
