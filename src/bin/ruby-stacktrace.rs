91cc9ed8ca976a5e6a327e7c1563fbaa238142fb
Julia Evans <julia@jvns.ca>
2018-01-16 11:36:50 -0500

    Implement "snapshot" and "record" features from README

--- src/bin/ruby-stacktrace.rs
+++ src/bin/ruby-stacktrace.rs
@@ -1,30 +1,146 @@
-#[macro_use]
 extern crate log;
 
+extern crate chrono;
 extern crate clap;
 extern crate env_logger;
+#[macro_use]
 extern crate failure;
 extern crate libc;
-extern crate read_process_memory;
+extern crate ruby_stacktrace;
+
 #[cfg(target_os = "macos")]
 extern crate regex;
-extern crate ruby_stacktrace;
-extern crate time;
-extern crate nix;
 
-use nix::sys::ptrace::*;
+use chrono::prelude::*;
 use clap::{App, AppSettings, Arg, ArgMatches};
-use libc::*;
+use libc::pid_t;
 use failure::Error;
 use failure::ResultExt;
-use std::process;
-use std::time::Duration;
-use std::thread;
-use std::collections::HashMap;
-use std::collections::HashSet;
-use std::process::Command;
 
 use ruby_stacktrace::*;
+
+fn do_main() -> Result<(), Error> {
+    env_logger::init().unwrap();
+
+    let matches: ArgMatches<'static> = arg_parser().get_matches();
+    let command = matches.value_of("SUBCOMMAND").unwrap();
+    let maybe_pid = matches.value_of("pid");
+    match command {
+        "snapshot" => {
+            let pid_string = maybe_pid.ok_or(format_err!("PID is required for snapshot option"))?;
+            let pid = pid_string
+                .parse()
+                .map_err(|_| format_err!("Invalid PID: {}", pid_string))?;
+            Ok(snapshot(pid)?)
+        }
+        "record" => {
+            let maybe_cmd = matches.values_of("cmd");
+            let maybe_filename = matches.value_of("file");
+            let pid: pid_t = match maybe_pid {
+                Some(x) => x.parse().map_err(|_| format_err!("Invalid PID: {}", x))?,
+                None => {
+                    exec_cmd(&mut maybe_cmd.expect("Either PID or command is required to record"))?
+                }
+            };
+            Ok(record(maybe_filename, pid)?)
+        }
+        x => Err(format_err!(
+            "'{}' is not a valid option: try 'snapshot' or 'record'",
+            x
+        )),
+    }
+}
+
+fn main() {
+    match do_main() {
+        Err(x) => {
+            println!("Error. Causes: ");
+            for c in x.causes() {
+                println!("- {}", c);
+            }
+            println!("{}", x.backtrace());
+            std::process::exit(1);
+        }
+        _ => {}
+    }
+}
+
+fn record(filename: Option<&str>, pid: pid_t) -> Result<(), Error> {
+    // This gets a stack trace and then just prints it out
+    // in a format that Brendan Gregg's stackcollapse.pl script understands
+    let process_info = user_interface::process_info(pid)?;
+    let mut output = open_record_output(filename)?;
+    let ruby_current_thread_address_location = process_info.current_thread_addr_location as u64;
+    let stack_trace_function = process_info.stack_trace_function;
+    loop {
+        let trace = stack_trace_function(ruby_current_thread_address_location, process_info.pid);
+        match trace {
+            Err(copy::MemoryCopyError::ProcessEnded) => return Ok(()),
+            Ok(ok_trace) => {
+                for t in ok_trace.iter().rev() {
+                    write!(output, "{}", t)?;
+                    write!(output, ";")?;
+                }
+                writeln!(output, " {}", 1)?;
+            }
+            Err(_) => {
+                println!("Dropping one stack trace.");
+            }
+        }
+        std::thread::sleep(std::time::Duration::from_millis(10));
+    }
+}
+
+fn snapshot(pid: pid_t) -> Result<(), Error> {
+    let process_info = user_interface::process_info(pid)?;
+    let stack_trace_function = process_info.stack_trace_function;
+    let trace = stack_trace_function(process_info.current_thread_addr_location, process_info.pid)?;
+    for x in trace.iter().rev() {
+        println!("{}", x);
+    }
+    Ok(())
+}
+
+fn open_record_output(maybe_filename: Option<&str>) -> Result<Box<std::io::Write>, Error> {
+    use std::os::unix::prelude::*;
+    match maybe_filename {
+        Some(filename) => {
+            let path = std::path::Path::new(filename);
+            println!("Recording data into {:?}...", path);
+            Ok(Box::new(std::fs::File::create(path)?))
+        }
+        None => {
+            let home = std::env::var("HOME")?;
+            let dt = Utc::now();
+            let date = dt.to_rfc3339();
+            let dirname = std::path::Path::new(&home)
+                .join(".rbspy")
+                .join("records")
+                .join(date);
+            std::fs::create_dir_all(&dirname)
+                .context(format!("Error creating directory {:?}", dirname))?;
+            let permissions = std::fs::Permissions::from_mode(0o777);
+            std::fs::set_permissions(&dirname, permissions.clone());
+            std::fs::set_permissions(
+                std::path::Path::new(&home).join(".rbspy"),
+                permissions.clone(),
+            );
+            std::fs::set_permissions(
+                std::path::Path::new(&home).join(".rbspy").join("record"),
+                permissions,
+            );
+            let path = dirname.join("stacks.txt");
+            println!("Recording data into {:?}...", path);
+            Ok(Box::new(std::fs::File::create(path)?))
+        }
+    }
+}
+
+fn exec_cmd(args: &mut std::iter::Iterator<Item = &str>) -> Result<pid_t, Error> {
+    let arg1 = args.next().unwrap();
+    let pid = std::process::Command::new(arg1).args(args).spawn()?.id() as pid_t;
+    Ok(pid)
+}
 
 fn arg_parser() -> App<'static, 'static> {
     App::new("ruby-stacktrace")
@@ -49,21 +165,12 @@ fn arg_parser() -> App<'static, 'static> {
         )
         .arg(
             Arg::from_usage(
-                "--pause 'Pause Ruby process before collecting stacktrace'",
-            ).required(false),
-        )
-        .arg(
-            Arg::from_usage(
                 "-p --pid=[PID] 'PID of the Ruby process you want to profile'",
             ).required_unless("cmd"),
         )
         .arg(Arg::from_usage("<cmd>... 'commands to run'").required(
             false,
         ))
-}
-
-fn parse_args() -> ArgMatches<'static> {
-    arg_parser().get_matches()
 }
 
 #[test]
@@ -86,134 +193,4 @@ fn test_arg_parsing() {
     assert!(cmd_values.next().unwrap() == "ruby");
     assert!(cmd_values.next().unwrap() == "blah.rb");
     assert!(result.value_of("SUBCOMMAND").unwrap() == "stackcollapse");
-}
-
-fn get_api_version(pid: pid_t) -> Result<String, Error> {
-    // this exists because sometimes rbenv takes a while to exec the right Ruby binary.
-    // we are dumb right now so we just... wait until it seems to work out.
-    let mut i = 0;
-    loop {
-        let version = address_finder::get_api_version(pid);
-        if i > 100 || version.is_ok() {
-            return Ok(version?);
-        }
-        // if it doesn't work, sleep for 1ms and try again
-        i += 1;
-        thread::sleep(Duration::from_millis(1));
-    }
-}
-
-fn timestamp() -> u64 {
-    let timespec = time::get_time();
-    let mills: u64 =
-        (timespec.sec as u64 * 1000 * 1000) as u64 + ((timespec.nsec as u64) / 1000 as u64);
-    mills
-}
-
-fn main() {
-    match do_main() {
-        Err(x) => {
-            println!("Error. Causes: ");
-            for c in x.causes() {
-                println!("- {}", c);
-            }
-            println!("{}", x.backtrace());
-            process::exit(1);
-        }
-        _ => {}
-    }
-}
-
-struct PtracePid {
-    pid: pid_t,
-    attached: bool,
-}
-
-impl PtracePid {
-    fn attach(&mut self) -> Result<(), Error> {
-        nix::sys::ptrace::ptrace(ptrace::PTRACE_ATTACH, nix::unistd::Pid::from_raw(self.pid), 0 as * mut c_void, 0 as * mut c_void)?;
-        self.attached = true;
-        Ok(())
-    }
-
-    fn detach(&mut self) -> Result<(), Error> {
-        if self.attached {
-            nix::sys::ptrace::ptrace(ptrace::PTRACE_DETACH, nix::unistd::Pid::from_raw(self.pid), 0 as * mut c_void, 0 as * mut c_void)?;
-            self.attached = false;
-        }
-        Ok(())
-    }
-}
-
-impl Drop for PtracePid {
-    fn drop(&mut self) {
-        self.detach().expect("detach failed");
-    }
-}
-
-fn do_main() -> Result<(), Error> {
-    env_logger::init().unwrap();
-
-    let matches = parse_args();
-    let command = matches.value_of("SUBCOMMAND").unwrap();
-    let pause = matches.occurrences_of("pause") == 1;
-    let maybe_pid = matches.value_of("pid");
-    let pid: pid_t = match maybe_pid {
-        Some(x) => x.parse().unwrap(),
-        None => {
-            let mut args = matches.values_of("cmd").unwrap();
-            let arg1 = args.next().unwrap();
-            let pid = Command::new(arg1)
-                .args(args)
-                .spawn()
-                .expect("command failed to start")
-                .id() as pid_t;
-            pid
-        }
-    };
-
-    let version = get_api_version(pid).context("Couldn't get API version")?;
-    debug!("version: {}", version);
-
-    if command.clone() != "top" && command.clone() != "stackcollapse" && command.clone() != "parse"
-    {
-        println!("COMMAND must be 'top' or 'stackcollapse. Try again!");
-        process::exit(1);
-    }
-
-    let ruby_current_thread_address_location =
-        address_finder::current_thread_address_location(pid, &version)?;
-    let stack_trace_function = stack_trace::get_stack_trace_function(&version);
-
-    let mut output: Box<std::io::Write> = match matches.value_of("file") {
-        Some(filename) => Box::new(std::fs::File::create(filename)?),
-        None => Box::new(std::io::stderr()),
-    };
-    if command == "parse" {
-        unimplemented!("parse command not implemented");
-    } else if command == "stackcollapse" {
-        // This gets a stack trace and then just prints it out
-        // in a format that Brendan Gregg's stackcollapse.pl script understands
-        loop {
-            {
-                let mut ptra = PtracePid{pid: pid, attached: false};
-                if pause {
-                    ptra.attach()?;
-                }
-                let trace = stack_trace_function(ruby_current_thread_address_location, pid);
-                match trace {
-                    Err(copy::MemoryCopyError::ProcessEnded) => return Ok(()),
-                    Ok(x) => {
-                        user_interface::print_stack_trace(&mut output, &x);
-                    }
-                    Err(y) => {
-                        println!("Dropping one stack trace.");
-                    }
-                }
-            }
-            thread::sleep(Duration::from_millis(10));
-        }
-    } else {
-        return Ok(())
-    }
 }
