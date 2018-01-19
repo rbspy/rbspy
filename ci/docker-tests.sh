d8da5085d244e201d92455b01ab292999d1f3425
Julia Evans <julia@jvns.ca>
2018-01-19 09:46:50 -0500

    add RUST_BACKTRACE=1 when running tests

--- ci/docker-tests.sh
+++ ci/docker-tests.sh
@@ -35,5 +35,5 @@ docker run -v=/tmp/artifacts:/stuff -t jvns/rbspy-ci:ubuntu1604 ls /root/.rbenv/
 for version in 2.1.0 2.1.1 2.1.2 2.1.3 2.1.4 2.1.5 2.1.6 2.1.7 2.1.8 2.1.9 2.1.10 2.2.0 2.2.1 2.2.2 2.2.3 2.2.4 2.2.5 2.2.6 2.2.7 2.2.8 2.2.9 2.3.0 2.3.1 2.3.2 2.3.3 2.3.4 2.3.5 2.3.6 2.4.0 2.4.1 2.4.2 2.4.3 2.5.0 1.9.3-p551
 do
    echo -n "${version}... "
-   docker run -v=/tmp/artifacts:/stuff -t jvns/rbspy-ci:ubuntu1604 /stuff/rbspy record --file /dev/null  /root/.rbenv/versions/$version/bin/ruby /stuff/short_program.rb
+   docker run -v=/tmp/artifacts:/stuff -t jvns/rbspy-ci:ubuntu1604 env RUST_BACKTRACE=1 /stuff/rbspy record --file /dev/null  /root/.rbenv/versions/$version/bin/ruby /stuff/short_program.rb
 done
