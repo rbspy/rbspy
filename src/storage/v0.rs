a8e3c31bba61bec0c47269f9b39a1e5e7eb7a4c1
Julia Evans <julia@jvns.ca>
2018-02-21 14:30:29 -0500

    Add PID to stack trace struct
    This updates the data storage format for rbspy to include a PID for each stack trace. The idea here is that sometimes we'll sample stack traces from multiple processes, so it's good to include a PID for each stack trace.

--- src/storage/v0.rs
+++ src/storage/v0.rs
@@ -1,29 +1,38 @@
 use std::io::prelude::*;
 use std::io::BufReader;
-use core::initialize::StackFrame;
+use core::initialize::{StackTrace, StackFrame};
 
 use failure::Error;
 use serde_json;
 
 use super::*;
 
-// The first version is just your internal representation.
-pub(crate) type Data = Vec<Vec<StackFrame>>;
+pub(crate) struct Data(Vec<Vec<StackFrame>>);
 
 impl Storage for Data {
     fn from_reader<R: Read>(r: R) -> Result<Data, Error> {
         let reader = BufReader::new(r);
         let mut result = Vec::new();
         for line in reader.lines() {
-            let trace = serde_json::from_str(&line?)?;
+            let trace: Vec<StackFrame> = serde_json::from_str(&line?)?;
             result.push(trace);
         }
-        Ok(result)
+        Ok(Data(result))
     }
     fn version() -> Version {
-        // The "version" for the internal representation is bumped every time
-        // you make a change to it, copying the old struct into a new file at
-        // `src/vblah`.
         Version(0)
+    }
+}
+
+impl From<Data> for v1::Data {
+    fn from(d: Data) -> v1::Data {
+        let x: Vec<StackTrace> = d.0.into_iter().map(|x| x.into()).collect();
+        v1::Data(x)
+    }
+}
+
+impl From<Vec<StackFrame>> for StackTrace {
+    fn from(trace: Vec<StackFrame>) -> StackTrace {
+        StackTrace{pid: None, trace}
     }
 }
---
+++ src/storage/v1.rs
@@ -0,0 +1,25 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use core::initialize::StackTrace;
+
+use failure::Error;
+use serde_json;
+
+use super::*;
+
+pub(crate) struct Data(pub Vec<StackTrace>);
+
+impl Storage for Data {
+    fn from_reader<R: Read>(r: R) -> Result<Data, Error> {
+        let reader = BufReader::new(r);
+        let mut result = Vec::new();
+        for line in reader.lines() {
+            let trace: StackTrace = serde_json::from_str(&line?)?;
+            result.push(trace);
+        }
+        Ok(Data(result))
+    }
+    fn version() -> Version {
+        Version(1)
+    }
+}
