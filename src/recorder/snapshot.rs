edd700a4138a70be1f4ce2b46594b231cfb58e7e
Adam Jensen <acjensen@gmail.com>
2021-08-22 11:47:39 -0400

    Simplify snapshot example

--- src/recorder/snapshot.rs
+++ src/recorder/snapshot.rs
@@ -1,11 +1,9 @@
 use crate::core::initialize::initialize;
-use crate::core::types::Pid;
+use crate::core::types::{Pid, StackTrace};
 use anyhow::{Error, Result};
 
 /// Captures a single trace from the process belonging to `pid`
-pub fn snapshot(pid: Pid, lock_process: bool) -> Result<String, Error> {
+pub fn snapshot(pid: Pid, lock_process: bool) -> Result<StackTrace, Error> {
     let mut getter = initialize(pid, lock_process)?;
-    let trace = getter.get_trace()?;
-    let frames: Vec<String> = trace.iter().rev().map(|s| s.to_string()).collect();
-    Ok(frames.join("\n"))
+    getter.get_trace()
 }
