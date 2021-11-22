e78290f0c4da86722795a8533724e1ba99b3579e
Adam Jensen <adam@acj.sh>
2021-11-21 21:54:30 -0500

    Migrate FreeBSD CI to vagrant

---
+++ ci/freebsd-build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+source ~/.bash_profile
+
+set -e
+
+ruby -v
+cargo --version
+
+export CARGO_HOME="/vagrant/.cargo"
+mkdir -p $CARGO_HOME
+
+cd /vagrant
+
+if [ -f build-artifacts.tar ]; then
+  tar xf build-artifacts.tar
+  rm -f build-artifacts.tar
+fi
+
+cargo build --release --workspace --all-targets
+cargo test --release -- \
+    --skip core::initialize::tests \
+    --skip sampler::tests
+
+tar cf build-artifacts.tar .cargo/{git,registry} target
