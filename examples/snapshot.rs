af07dccaab5c03ff495e944c364a7ef2deb9a39c
Ivan Zvonimir Horvat <ivanzvonimirhorvat@gmail.com>
2022-04-09 23:19:04 +0200

    include mod in examples

--- examples/snapshot.rs
+++ examples/snapshot.rs
@@ -1,6 +1,8 @@
+mod include;
 extern crate rbspy;
 
 use rbspy::recorder::snapshot;
+use crate::include::path_to_ruby_binary;
 
 fn main() {
     let mut process = std::process::Command::new(path_to_ruby_binary())
@@ -15,26 +17,4 @@ fn main() {
     }
 
     process.kill().expect("couldn't clean up ruby process");
-}
-
-fn path_to_ruby_binary() -> String {
-    let which = if cfg!(target_os = "windows") {
-        "C:\\Windows\\System32\\WHERE.exe"
-    } else {
-        "/usr/bin/which"
-    };
-
-    let output = std::process::Command::new(which)
-        .arg("ruby")
-        .output()
-        .expect("failed to execute process");
-
-    let ruby_binary_path = String::from_utf8(output.stdout).unwrap();
-
-    let ruby_binary_path_str = ruby_binary_path
-        .lines()
-        .next()
-        .expect("failed to execute ruby process");
-
-    ruby_binary_path_str.to_string()
 }
