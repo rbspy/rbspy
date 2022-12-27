24ad81d2439f9e63dd91cc1126ca1bb5d3a4da5b
Adam Jensen <adam@acj.sh>
2022-12-26 20:27:32 -0500

    Fix typo in expectation message

--- src/ui/summary.rs
+++ src/ui/summary.rs
@@ -160,7 +160,7 @@ mod tests {
 ";
 
         let mut buf: Vec<u8> = Vec::new();
-        stats.write(&mut buf).expect("Callgrind write failed");
+        stats.write(&mut buf).expect("summary write failed");
         let actual = String::from_utf8(buf).expect("summary output not utf8");
         assert_eq!(actual, expected, "Unexpected summary output");
     }
