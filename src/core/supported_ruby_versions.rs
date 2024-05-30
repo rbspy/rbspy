bf66c0cb83b8153e7f1062ae5cdfedc75da3c966
Adam Jensen <adam@acj.sh>
2024-05-29 21:28:35 -0400

    Add support for ruby 3.1.6 and 3.3.2

--- src/core/supported_ruby_versions.rs
+++ src/core/supported_ruby_versions.rs
@@ -550,6 +550,12 @@ pub fn get(v: &str) -> Result<RubyVersion> {
             get_stack_trace_fn: super::ruby_version::ruby_3_1_5::get_stack_trace,
             is_maybe_thread_fn: super::ruby_version::ruby_3_1_5::is_maybe_thread,
         }),
+        "3.1.6" => Ok(RubyVersion {
+            semver_version: Version::new(3, 1, 6),
+            get_execution_context_fn: super::ruby_version::ruby_3_1_6::get_execution_context,
+            get_stack_trace_fn: super::ruby_version::ruby_3_1_6::get_stack_trace,
+            is_maybe_thread_fn: super::ruby_version::ruby_3_1_6::is_maybe_thread,
+        }),
         "3.2.0" => Ok(RubyVersion {
             semver_version: Version::new(3, 2, 0),
             get_execution_context_fn: super::ruby_version::ruby_3_2_0::get_execution_context,
@@ -591,6 +597,12 @@ pub fn get(v: &str) -> Result<RubyVersion> {
             get_execution_context_fn: super::ruby_version::ruby_3_3_1::get_execution_context,
             get_stack_trace_fn: super::ruby_version::ruby_3_3_1::get_stack_trace,
             is_maybe_thread_fn: super::ruby_version::ruby_3_3_1::is_maybe_thread,
+        }),
+        "3.3.2" => Ok(RubyVersion {
+            semver_version: Version::new(3, 3, 2),
+            get_execution_context_fn: super::ruby_version::ruby_3_3_2::get_execution_context,
+            get_stack_trace_fn: super::ruby_version::ruby_3_3_2::get_stack_trace,
+            is_maybe_thread_fn: super::ruby_version::ruby_3_3_2::is_maybe_thread,
         }),
         _ => Err(format_err!("rbspy doesn't support Ruby {} yet. If this is a new patch-level version of Ruby, you can try using `--force-version` with the previous version number.", v)),
     }
