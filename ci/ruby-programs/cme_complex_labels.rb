70c01cf44f77d1ee9a8bdae339787934db897bd4
Dale Hamel <dale.hamel@shopify.com>
2025-10-20 15:15:53 -0400

    Style nits in test program

--- ci/ruby-programs/cme_complex_labels.rb
+++ ci/ruby-programs/cme_complex_labels.rb
@@ -1,14 +1,14 @@
 require_relative './interesting_backtrace_helper'
 
 class InstanceMethod
-  def work()
+  def work
     top_level_hello
   end
 end
 
 module ModuleMethod
-  def call_instance()
-      InstanceMethod.new.work()
+  def call_instance
+    InstanceMethod.new.work
   end
 end
 
@@ -17,10 +17,10 @@ class IncludedMethod
 end
 
 # Top level method test too (block in main)
-def work_main()
-  IncludedMethod.new.call_instance()
+def work_main
+  IncludedMethod.new.call_instance
 end
 
 loop do
-    work_main()
+  work_main
 end
